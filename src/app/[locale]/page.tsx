139d0e6eaf6e62c7bc2c7c224499a68e6f522c2d
EnzoComics <enzo@cheerupemokid.com>
2023-12-15 03:19:18 -0800

    updated css variables & added visual examples

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -17,6 +17,43 @@ export default async function Homepage() {
 			<div className="font-copy">
 				{t('message')}
 			</div>
+			<ul>
+				<li>
+					<button className="bg-primary text-colors-content">
+						Primary
+					</button>
+				</li>
+				<li>
+					<button className="bg-secondary text-colors-content">
+						Secondary
+					</button>
+				</li>
+				<li>
+					<button className="bg-error text-colors-content">
+						Error
+					</button>
+				</li>
+				<li>
+					<button className="bg-base-100">
+						Base-100
+					</button>
+				</li>
+				<li>
+					<button className="bg-base-200">
+						Base-200
+					</button>
+				</li>
+				<li>
+					<button className="bg-base-300">
+						Base-300
+					</button>
+				</li>
+				<li>
+					<button className="bg-base-content text-base-invert">
+						Base inverted
+					</button>
+				</li>
+			</ul>
 		</>
 	)
 }
